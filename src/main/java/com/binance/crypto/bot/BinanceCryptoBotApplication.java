0c0c4b61e309ff40db5a7271557a6f730f64e2bc
Alens Krseminskis <alenkrse@gmail.com>
2023-03-18 07:57:21 +0200

    init project

---
+++ src/main/java/com/binance/crypto/bot/BinanceCryptoBotApplication.java
@@ -0,0 +1,13 @@
+package com.binance.crypto.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceCryptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceCryptoBotApplication.class, args);
+	}
+
+}
